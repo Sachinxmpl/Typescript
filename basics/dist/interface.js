2fa9edf420bfca46ee6f364cbec379ecd8d5a178
Sachinxmpl <sachinxmpl6@gmail.com>
2024-08-23 18:41:02 +0545

    complete interface

--- basics/dist/interface.js
+++ basics/dist/interface.js
@@ -1,10 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const getArray = (person) => {
-    return Object.assign({}, person);
+const sachin = {
+    githubId: 234,
+    email: "kckjdlfsadf",
+    userId: 1,
+    trail: () => {
+        return "console.log";
+    },
+    getCoupon: (iscode, name) => {
+        return name + iscode.toString();
+    }
 };
-const person1 = {
-    name: "sachin",
-    age: 2342
+const person3 = {
+    smalluser: false,
+    email: "email",
+    githubId: 234,
+    userId: 1,
+    trail: () => {
+        return "console.log";
+    },
+    getCoupon: (iscode, name) => {
+        return name + iscode.toString();
+    }
 };
-console.log(getArray(person1));
+console.log(person3.trail());
+//!some advantage of interface in typescirpt 
+//* We can extend interface  (inheritanve similar to classes)
+//* Reopening of interface is possible  ,, type cannot be changed after creating 
+//* defining function prototypes  , other types in a single interface is possible
--- basics/src/interface.ts
+++ basics/src/interface.ts
@@ -1,17 +1,56 @@
-interface person {
-    name : string , 
-    age : number 
-} 
-
-const getArray  = (person : person )  : {}=>{
-    return {...person}
+//we can also define a function prototype 
+interface User {
+    email : string , 
+    readonly userId : number 
+    trail : () => string  
+    getCoupon : (code : number , name : string) => string
 }
 
-const person1 : person  = {
-    name : "sachin" , 
-    age : 2342
+//we can also add interface User again
+//!reopening of interface 
+interface User {
+    githubId : number 
 }
 
-console.log(getArray(person1))
+const sachin :User = {
+    githubId : 234 , 
+    email : "kckjdlfsadf" , 
+    userId : 1 , 
+    trail : ()=>{
+        return  "console.log"
+    } , 
+    getCoupon : (iscode , name ) => {
+        return name + iscode.toString()
+    }
+}
+
+
+interface Smalluser extends User  {
+    smalluser : boolean 
+}
+
+const person3 : Smalluser = {
+    smalluser : false , 
+    email : "email" , 
+    githubId : 234 , 
+    userId : 1 , 
+    trail : ()=>{
+        return  "console.log"
+    } , 
+    getCoupon : (iscode , name ) => {
+        return name + iscode.toString()
+    }
+}
+
+console.log(person3.trail())
+
+// console.log(sachin.trail())
 
 export {}
+
+
+
+//!some advantage of interface in typescirpt 
+//* We can extend interface  (inheritanve similar to classes)
+//* Reopening of interface is possible  ,, type cannot be changed after creating 
+//* defining function prototypes  , other types in a single interface is possible
